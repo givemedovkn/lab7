b145ed60cd31be1a373c7efb9a2fb8c493f11492
UsyninaVika <vikadog6002@gmail.com>
2026-04-22 03:42:38 +0500

    Add files via upload

---
+++ Toy.cs
@@ -0,0 +1,66 @@
+﻿[Serializable]
+public struct Toy
+{
+    private string name;
+    private double price;
+    private int minAge;
+    private int maxAge;
+
+    public Toy(string name, double price, int minAge, int maxAge)
+    {
+        this.name = name;
+        this.price = price;
+        this.minAge = minAge;
+        this.maxAge = maxAge;
+    }
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+    public double Price
+    {
+        get
+        {
+            return price;
+        }
+        set
+        {
+            price = value;
+        }
+    }
+    public int MinAge
+    {
+        get
+        {
+            return minAge;
+        }
+        set
+        {
+            minAge = value;
+        }
+    }
+    public int MaxAge
+    {
+        get
+        {
+            return maxAge;
+        }
+        set
+        {
+            maxAge = value;
+        }
+    }
+    public override string ToString()
+    {
+        return "Игрушка " + name + ", цена = " + price
+            + ", возраст от " + minAge + " до " + maxAge;
+
+    }
+}
